1cb588c48a582de24a9355530d0a875347b481cb
Ezz dghedy <ezzmohsend@gmail.com>
2024-03-11 22:13:23 +0200

    infra structure setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'flowbite-react';
+
+function App() {
+  return (
+    <div>
+      <Button className="btn btn-accent btn-block bg-secondary underline text-5xl">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
