a41110a2d01746d8f24ddcd94ed74bb77bfc7445
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-07 13:31:28 +0200

    finishing the hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,23 +10,23 @@ const Header = () => {
   const closeDrawer = () => setOpen(false);
 
   return (
-    <div className="w-full fixed top-0 left-0 bg-transparent flex items-center justify-start border-b-2 border-b-[#ddd] ">
+    <div className="w-[100vw] fixed top-0 left-0 bg-transparent flex items-center justify-start border-b-2 border-b-[#ddd] ">
       <button
         onClick={openDrawer}
-        className="h-[95px] w-[20vw] md:w-[15vw] bg-inherit flex justify-center items-center text-white gap-2 hover:bg-[#9b0808] active:bg-[#9b0808] border-r-2 border-r-[#ddd] border-spacing-2"
+        className="h-[95px] w-[25vw] md:w-[15vw] bg-inherit flex justify-center items-center text-white gap-2 hover:bg-[#9b0808] active:bg-[#9b0808] border-r-2 border-r-[#ddd] border-spacing-2 bg-[#9b0808] lg:bg-transparent"
       >
-        <FaBars className="w-4 h-4 md:w-6 md:h-6" />
+        <FaBars className="w-4 h-4 md:w-6 md:h-6 text-md md:text-xl" />
         <h1 className="text-md md:text-xl">MENU</h1>
       </button>
       {/* DRAWER */}
       <TheDrawer open={open} closeDrawer={closeDrawer} />
 
       {/* Drawer End */}
-      <div className="w-[60vw] md:w-[70vw] flex justify-center items-center">
+      <div className="w-[50vw] md:w-[70vw] flex justify-center items-center">
         <img src={logo} alt="logo" />
       </div>
-      <div className="w-[20vw] md:w-[15vw] h-[95px] flex text-white justify-center items-center gap-4 hover:bg-[#9b0808] active:bg-[#9b0808] border-l-2 border-l-[#ddd] border-spacing-2">
-        <MdLock className="w-4 h-4 md:w-6 md:h-6" />
+      <div className="w-[25vw] md:w-[15vw] h-[95px] flex text-white justify-center items-center gap-4 hover:bg-[#9b0808] active:bg-[#9b0808] border-l-2 border-l-[#ddd] border-spacing-2">
+        <MdLock className="w-4 h-4 md:w-6 md:h-6 text-md md:text-xl" />
         <h1 className="text-md md:text-xl">LOGIN</h1>
       </div>
     </div>
