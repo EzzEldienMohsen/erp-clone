c778b6ba40b1069e2c3ec6846c68fcbdc6ce1415
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-12 15:47:46 +0200

    landing modules done

--- src/SubComponents/LandingModule.jsx
+++ src/SubComponents/LandingModule.jsx
@@ -1,35 +1,29 @@
-import { buttonModule, landingModules } from "../assets";
+import React from "react";
+import { buttonModule } from "../assets";
+import { motion, AnimatePresence } from 'framer-motion';
 
 const LandingModule = () => {
-  const toggleThings = (id,bId,c,activeClass)=>{
-    const buttons = document.querySelectorAll(".btn")
-    const button = document.getElementById(bId);
-    const cards = document.querySelectorAll("section")
-    const card = document.getElementById(id)
-buttons.forEach((button) => button.classList.remove(`${activeClass}-${bId}`));
-   button.classList.add(`${activeClass}-${bId}`);
-    cards.forEach((card)=>card.classList.add("hidden"))
-    card.classList.remove("hidden")
-    
-  }
+  const [selectedButton,setSelectedButton] = React.useState("a")
+  const [module,setModule] = React.useState(buttonModule[0].module)
+
   return (
     <div className>
       <div className=" md:h-[130px] sm:px-0 md:px-10 grid grid-cols-2 md:flex md:flex-row bg-[#f5f5f5]">
-        {buttonModule.map((button)=>{
+        {buttonModule.map((button) => {
           return (
             <button
               key={button.id}
               id={button.id}
-              onClick={() =>{
-                toggleThings(
-                  button.cardId,
-                  button.id,
-                  button.class,
-                  button.activeClass
-                )               
-                }
-              }
-              className={`btn items-end  ${button.class}-${button.id}  hover:text-[#9b0808] justify-center btn-square w-[50vw] md:w-1/4  h-[130px] border-1 border-gray-300 rounded-none  bg-no-repeat bg-[length:auto_95px] bg-center `}
+              onClick={() => {
+               
+                setSelectedButton(button.id);
+                setModule(button.module);
+              }}
+              className={`btn items-end border-t-2 ${
+                button.id === selectedButton
+                  ? `${button.activeClass}-${button.id}`
+                  : `${button.class}-${button.id}`
+              }  hover:text-[#9b0808] justify-center btn-square w-[50vw] md:w-1/4  h-[130px] border-1 border-gray-300 rounded-none  bg-no-repeat bg-[length:auto_95px] bg-center `}
             >
               <span className="mt-2 mb-1 font-light">{button.text}</span>
             </button>
@@ -37,28 +31,43 @@ buttons.forEach((button) => button.classList.remove(`${activeClass}-${bId}`));
         })}
       </div>
       <div className="bg-white text-[#333] font-light p-10 flex flex-col justify-start items-start">
-        {landingModules.map((module)=>{
-          return (
-            <section key={module.id} id={module.id} className="hidden">
-              <h1 className="text-lg md:text:xl lg:text-2xl mb-2">{module.title}</h1>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={module ? module.id : 'empty'}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <h1 className="text-lg md:text:xl lg:text-2xl mb-2">
+                {module.title}
+              </h1>
               <p className="mb-2">{module.firstText}</p>
               <div className="flex flex-col md:flex-row justify-between gap-6">
-                <img src={module.img} alt="img" className="w-1/2 mr-6" />
+                <img src={module.img} alt="img" className="w-3/5 mr-4" />
                 <div className="flex flex-col ">
                   <h3 className="mb-2">{module.secondText}</h3>
-                  <ul className="list-disc gap-2 text-[#9b0808]">
-                    {module.list.map((list)=>{
-                      return <li key={list} className="my-1">{list}</li>
+                  <ul className="list-disc gap-2 ml-3 text-[#9b0808]">
+                    {module.list.map((list) => {
+                      return (
+                        <li key={list} className="my-1">
+                          {list}
+                        </li>
+                      );
                     })}
                   </ul>
                 </div>
               </div>
-            </section>
-          );
-        })}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );
 }
 
 export default LandingModule
+
+
+
+
+ 
--- src/assets/index.js
+++ src/assets/index.js
@@ -13,6 +13,22 @@ export const logo =
       class: 'Button',
       activeClass: 'activeButton',
       text: 'Human Capital Management',
+      module: {
+        id: 1,
+        title: 'Human Capital Management',
+        firstText:
+          'Full-service Human Capital Managements for small- to medium- sized businesses operating in a single country. With Ratiba Enterprise, CRM module you benefit from a rich array of modules to suit the specific needs of your company and your budget. For businesses growing out of Ratiba Enterprise, or for companies that need a comprehensive HR platform, Ratiba Enterprise Professional offers superior management and cost inefficiencies across the board.',
+        secondText:
+          'We include the modules below with every Ratiba Enterprise CRM package;',
+        img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b317c680771774991f39_system_admin-p-800x554.png',
+        list: [
+          'System Administration & User Roles',
+          'Leave /Time off Management',
+          'Training',
+          'Cloud Environment',
+          'Document Manager',
+        ],
+      },
     },
     {
       id: 'b',
@@ -20,6 +36,20 @@ export const logo =
       class: 'Button',
       activeClass: 'activeButton',
       text: 'Payroll Management',
+      module: {
+        id: 2,
+        title: 'Payroll Manager',
+        firstText:
+          'Ratiba Enterprise Payroll Manager is the complete, full-service solution for managing your payroll. We do all the hard work for you, including handling all filings for NSSF, NHIF, PAYE, pension schemes and other customized check off deductions. The Ratiba Enterprise Payroll Service minimizes payroll headaches, reduces tedious paperwork, and gives you a peace of mind.',
+        secondText:
+          'Payroll Management has never been this easy. We have ensured that you will never need to prepare or mail payroll tax forms again. Everything is simplified under three key features;',
+        img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b31730ec952f432f6cc1_payroll_manager-p-800x554.png',
+        list: [
+          'Scalable Integration',
+          'Hand On Analytics/ Data Insights',
+          'Document Manager',
+        ],
+      },
     },
     {
       id: 'c',
@@ -27,76 +57,43 @@ export const logo =
       class: 'Button',
       activeClass: 'activeButton',
       text: 'Workshop Module',
+      module: {
+        id: 3,
+        title: 'Workshop Module',
+        firstText:
+          'Ratiba Enterprise ASSET Manager, allows you can track an unlimited number of pieces of equipment. Making management easier than ever, since you can store a description, make, model, serial number, photos, and more for each piece of equipment. More than 20 custom fields provide the flexibility you need to store information unique to your array of equipment. If using meter or time tracking, you can select from hours, kilometers, counts, copies, or define your own customized meter type for any piece of equipment.',
+        secondText: 'The Asset Manager carries these features among many more;',
+        img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b317d462ab2443989c5a_workshop_module-p-800x554.png',
+        list: [
+          'Preventive Maintenance',
+          'Repair Maintenance',
+          'Due Maintenance Notifications',
+          'Inventory Management',
+          'Work Order Management',
+          'Compliance Management',
+        ],
+      },
     },
     {
       id: 'd',
-      cardId:4,
+      cardId: 4,
       class: 'Button',
       activeClass: 'activeButton',
       text: 'Journey Management',
-    },
-  ];
-
-  export const landingModules = [
-    {
-      id: 1,
-      title: 'Human Capital Management',
-      firstText:
-        'Full-service Human Capital Managements for small- to medium- sized businesses operating in a single country. With Ratiba Enterprise, CRM module you benefit from a rich array of modules to suit the specific needs of your company and your budget. For businesses growing out of Ratiba Enterprise, or for companies that need a comprehensive HR platform, Ratiba Enterprise Professional offers superior management and cost inefficiencies across the board.',
-      secondText:
-        'We include the modules below with every Ratiba Enterprise CRM package;',
-      img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b317c680771774991f39_system_admin-p-800x554.png',
-      list: [
-        'System Administration & User Roles',
-        'Leave /Time off Management',
-        'Training',
-        'Cloud Environment',
-        'Document Manager',
-      ],
-    },
-    {
-      id: 2,
-      title: 'Payroll Manager',
-      firstText:
-        'Ratiba Enterprise Payroll Manager is the complete, full-service solution for managing your payroll. We do all the hard work for you, including handling all filings for NSSF, NHIF, PAYE, pension schemes and other customized check off deductions. The Ratiba Enterprise Payroll Service minimizes payroll headaches, reduces tedious paperwork, and gives you a peace of mind.',
-      secondText:
-        'Payroll Management has never been this easy. We have ensured that you will never need to prepare or mail payroll tax forms again. Everything is simplified under three key features;',
-      img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b31730ec952f432f6cc1_payroll_manager-p-800x554.png',
-      list: [
-        'Scalable Integration',
-        'Hand On Analytics/ Data Insights',
-        'Document Manager',
-      ],
-    },
-    {
-      id: 3,
-      title: 'Workshop Module',
-      firstText:
-        'Ratiba Enterprise ASSET Manager, allows you can track an unlimited number of pieces of equipment. Making management easier than ever, since you can store a description, make, model, serial number, photos, and more for each piece of equipment. More than 20 custom fields provide the flexibility you need to store information unique to your array of equipment. If using meter or time tracking, you can select from hours, kilometers, counts, copies, or define your own customized meter type for any piece of equipment.',
-      secondText: 'The Asset Manager carries these features among many more;',
-      img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b317d462ab2443989c5a_workshop_module-p-800x554.png',
-      list: [
-        'Preventive Maintenance',
-        'Repair Maintenance',
-        'Due Maintenance Notifications',
-        'Inventory Management',
-        'Work Order Management',
-        'Compliance Management',
-      ],
-    },
-    {
-      id: 4,
-      title: 'Journey Management',
-      firstText:
-        'Ratiba Journey Management Suite provides the greatest visibility into the real-time workings of field staff and gives business owners a dynamic tool to ensure that each system is working together to guarantee the most efficient and accurate performance in the field. Ratiba offers real-time job creation, tracking (including estimated time of arrival and completion), routing (including pre-planned routes and dynamic changes throughout the day), dispatching, customer service notifications, route compliance reporting, journey completion reporting.',
-      secondText:
-        'We have made Journey Management easy to use by debloating the interface and adding powerful features to compliment you work flow. They are;',
-      img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b31785a476a775f410be_journey_manager-p-800x554.png',
-      list: [
-        'Easy System Configuration',
-        'End-to End Process',
-        'Consistent User Experience',
-        'Disciplinary Tracking',
-      ],
+      module: {
+        id: 4,
+        title: 'Journey Management',
+        firstText:
+          'Ratiba Journey Management Suite provides the greatest visibility into the real-time workings of field staff and gives business owners a dynamic tool to ensure that each system is working together to guarantee the most efficient and accurate performance in the field. Ratiba offers real-time job creation, tracking (including estimated time of arrival and completion), routing (including pre-planned routes and dynamic changes throughout the day), dispatching, customer service notifications, route compliance reporting, journey completion reporting.',
+        secondText:
+          'We have made Journey Management easy to use by debloating the interface and adding powerful features to compliment you work flow. They are;',
+        img: 'https://assets.website-files.com/57dee8d6b08345807bd940be/57e8b31785a476a775f410be_journey_manager-p-800x554.png',
+        list: [
+          'Easy System Configuration',
+          'End-to End Process',
+          'Consistent User Experience',
+          'Disciplinary Tracking',
+        ],
+      },
     },
   ];
