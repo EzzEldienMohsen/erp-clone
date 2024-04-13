61e2dd1756b2035af7fb536c0c42477b2db9b471
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-13 17:20:20 +0200

    footer adj done

--- src/SubComponents/FooterContactInfo.jsx
+++ src/SubComponents/FooterContactInfo.jsx
@@ -1,18 +1,17 @@
 
 const FooterContactInfo = () => {
   return (
-    <div className="flex flex-col gap-3 md:gap-6 justify-center items-start mt-8 lg:mt-0">
-      <h3 className="text-black font-normal capitalize sm:text-lg md:text-xl lg:text-2xl  lg:mt-8">
-        Contact
-      </h3>
-      <ul className="text-[#6f6f6f] font-light capitalize ">
-        <li className="mt-3">ccare@martsgroup.org</li>
-        <li className="mt-3">+254 020 2249556</li>
-        <li className="">+254 755 237588</li>
-        <li className="mt-3">6th Floor, Chai House, Nairobi, Kenya.</li>
-      </ul>
-    </div>
-  );
+          <div className=" lg:w-[3-0%] flex flex-col gap-3 md:gap-6 justify-center items-start mt-8 lg:mt-0">
+            <h3 className="text-black font-normal capitalize sm:text-lg md:text-xl lg:text-2xl  lg:mt-8">
+              Contact
+            </h3>
+            <ul className="text-[#6f6f6f] font-light capitalize ">
+              <li className="mt-3">ccare@martsgroup.org</li>
+              <li className="mt-3">+254 020 2249556</li>
+              <li className="">+254 755 237588</li>
+              <li className="mt-3">6th Floor, Chai House, Nairobi, Kenya.</li>
+            </ul>
+          </div>  )
 }
 
 export default FooterContactInfo
