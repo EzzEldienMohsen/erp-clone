88e312c66e7947bd20b245ab4df2f23b05b4fe66
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-14 18:22:20 +0200

    module page hero

--- src/pages/ModulePage.jsx
+++ src/pages/ModulePage.jsx
@@ -12,13 +12,20 @@ const moduleData = (id) => {
 export const loader = (queryClient)=> async ({params})=> {
     const {id} = params;
     const data = await queryClient.ensureQueryData(moduleData(id));
-    console.log(data)
+    console.log(data.data)
     return data
 } ;
 
 const ModulePage = () => {
     const { data } = useLoaderData();
-  return <div>{data.data}</div>;
+    const {title,id,text,theClass,content,cards} = data
+    console.log(data.data)
+  return <div className="box-border w-[100vw]">
+    <div className={`w-full ${theClass} flex justify-center items-center h-[30vh] md:h-[70vh] lg:h-[50vh]`}>
+        <h3 className='text-white uppercase text-xl md:text-2xl lg:text-4xl mt-8'>{title}</h3>
+        
+    </div>
+  </div>;
 };
 
 export default ModulePage;
