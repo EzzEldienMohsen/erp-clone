706529e8d56a5f727bd9d10943fa76683300b19e
Ezz dghedy <ezzmohsend@gmail.com>
2024-04-16 11:56:17 +0200

    vercel env

--- src/SubComponents/TheMap.jsx
+++ src/SubComponents/TheMap.jsx
@@ -3,9 +3,9 @@ import {
   AdvancedMarker,
   Map,
 } from '@vis.gl/react-google-maps';
-const key = import.meta.env.VITE_API_KEY;
+const key = import.meta.env.VITE_API_DOOR;
 const mapId = import.meta.env.VITE_MAP_ID;
-
+console.log(key)
 
 const TheMap = () => {
         // style={{ width: '40vw', height: '40vh' }}
